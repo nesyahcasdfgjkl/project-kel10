5a601b33d65b95e65e31c9aecec7b744cd704c28
nadiaarianesyaa <102537045+nesyahcasdfgjkl@users.noreply.github.com>
2024-06-16 12:10:25 +0700

    insert-data-countries

--- insert-data.sql
+++ insert-data.sql
@@ -3,3 +3,12 @@ INSERT INTO tbl_regions (id, name) VALUES
 (1, 'North America'),
 (2, 'Europe'),
 (3, 'Asia');
+
+-- Insert into tbl_countries
+INSERT INTO tbl_countries (id, name, region) VALUES 
+('USA', 'United States', 1),
+('CAN', 'Canada', 1),
+('GBR', 'United Kingdom', 2),
+('DEU', 'Germany', 2),
+('CHN', 'China', 3),
+('JPN', 'Japan', 3);

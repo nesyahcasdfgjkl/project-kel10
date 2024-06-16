e740ea220b1c2591e73c6733f68c5c6c479f34ea
selvshaf <156678363+selvshaf@users.noreply.github.com>
2024-06-16 23:57:34 +0700

    Update usp_addRegiont.sql

--- usp_addRegiont.sql
+++ usp_addRegiont.sql
@@ -1,13 +1,56 @@
-CREATE PROCEDURE AddRegion
-    @Id INT,
-    @Name VARCHAR(25)
+CREATE OR ALTER PROCEDURE AddNewRegions
+   	@idReg INT,
+	@nameReg varchar(40)
 AS
 BEGIN
-    INSERT INTO tbl_regions (id, name)
-    VALUES (@Id, @Name);
+	DECLARE @message VARCHAR(40)
+	SET @message = 'Terdapat duplikat data id regions, silahkan cek kembali';
+-- Isi dari stored procedure
+	IF NOT EXISTS (SELECT 1 FROM tbl_regions WHERE id = @idReg)
+	BEGIN
+		INSERT INTO tbl_regions( id,name)
+		VALUES (@idReg,@nameReg);
+--setelah insert akan ditampilkan datanya
+		SELECT id,name FROM tbl_reg Where id=@idReg;
+	END
+	ELSE
+	BEGIN
+		PRINT @message
+	END
 END;
-GO
 
-EXEC AddRegion @Id = 5, @Name = 'Oceania';
+--Call Store Procedur
+EXEC AddNewRegions 8,'change';
 
 select * from tbl_regions;
+
+
+--update regions
+CREATE OR ALTER PROCEDURE UpdateRegions
+    	@idReg INT,
+	@nameReg varchar(100)
+AS
+BEGIN
+	DECLARE @message VARCHAR(50);
+    SET @message = 'Inputan id salah!!';
+   
+
+    IF NOT EXISTS (SELECT 1 FROM tbl_regions WHERE id = @idReg)
+    BEGIN
+        PRINT @message;
+    END
+    ELSE
+    BEGIN
+		UPDATE tbl_regions
+		SET
+        id = @idReg,
+        name = @nameReg
+		WHERE id=@idReg;
+        SELECT * FROM tbl_regions WHERE id = @idReg;
+    END
+END;
+
+--call update
+EXEC UpdateRegions 7,'Pacific';
+EXEC UpdateRegions 8,'LATAM';
+
